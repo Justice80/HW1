1cfdda9e3fce938fc7c8216f125239a110d27d10
Justice Martinez <amartinez18@my.whitworth.edu>
2015-05-20 22:09:59 -0700

    HW#1
    Homework #1 file, has all problems in one solution.

---
+++ EX01.cpp
@@ -0,0 +1,35 @@
+/* Name: Justice Martinez
+   Date: 5/20/2015
+   Assignment #1
+   CS 172 */
+
+/* EX 01_01:
+   a) Central Processing Unit
+   b) Main Memory
+   c) i) on a disk
+   d) v) a binary digit, like 0 or 1
+   e) Address
+   f) Eight
+   g) iv) allocates resources like memory to programs that are running
+   h) ii) is any software that is runs with the support of the operating system.  
+   */
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include "Functions.h"
+#include "OtherFunctions.h"
+using namespace std;
+
+int main() {
+
+	srand(time(NULL));
+	ex02();
+	ex03();
+	ex04();
+	ex05();
+
+	return 0;
+}
